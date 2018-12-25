0b9cd4b9f5cbcd86f8eeb4f4cebc4c12b40d3ddf
kovner chalumo <kchalumo@gmail.com>
2018-12-25 03:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidaysTo(name) { return 'Happy holidays, ${name}!'; }
 function happyHolidays(name) { return "Happy holidays!"; }
-function HappyHolidayTo(holiday, name) { return 'Happy ${holiday}, ${name}!';
+function HappyHolidayTo(holiday, name) { return "Happy ${holiday}, ${name}!";
 }
 function holidayCountdown(holiday, days) { return 'It\'s ${days} days until ${holiday}!';
 }
